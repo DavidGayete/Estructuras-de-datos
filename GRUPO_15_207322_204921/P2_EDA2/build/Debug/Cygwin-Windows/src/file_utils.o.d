6e7ca1002b0cd9764757810d51eeeed4007d7214
DavidGayete <80595030+DavidGayete@users.noreply.github.com>
2021-03-15 17:04:36 +0100

    Delete GRUPO_15_207322_204921 directory

--- GRUPO_15_207322_204921/P2_EDA2/build/Debug/Cygwin-Windows/src/file_utils.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/Cygwin-Windows/src/file_utils.o: src/file_utils.c \
- src/file_utils.h src/word_info.h
-
-src/file_utils.h:
-
-src/word_info.h:
--- GRUPO_15_207322_204921/P2_EDA2/build/Debug/Cygwin-Windows/src/word_info.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/Cygwin-Windows/src/word_info.o: src/word_info.c \
- src/word_info.h src/file_utils.h
-
-src/word_info.h:
-
-src/file_utils.h:
